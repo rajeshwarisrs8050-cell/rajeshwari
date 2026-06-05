d2bb2cbb56b0a9a8cb2bb34000c7a9e0e6e1931d
rajeshwarisrs8050-cell <rajeshwarisrs8050@gmail.com>
2026-06-05 10:35:23 +0530

    Update demo.java

--- demo.java
+++ demo.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class ErrorHandlingDemo {
+public class ErrorHandlingDemo (
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
